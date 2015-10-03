45f266fad2beed83acbc63aae7ae6a9b09efa47d
Alex Crits-Christoph <acritsc1@jhu.edu>
2015-10-03 17:57:47 -0400

    Adding example data

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import json
+from flask import jsonify
+
 
 app = Flask(__name__)
 
@@ -7,8 +11,11 @@ app = Flask(__name__)
 def main_page():
     return render_template('index.html')
 
+@app.route('/search', methods=['POST'])
 def search_graph():
-	return "Hi!"
+	print "GOT HERE"
+	query = request.form['query']
+	return jsonify({"results":query})
 
 if __name__ == "__main__":
     app.run(debug=True)
